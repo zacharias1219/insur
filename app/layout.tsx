ec05ec8095c342e8d45efeb688b69848ffe52789
Richard Abishai <rickoshade1891@gmail.com>
2024-11-04 15:07:38 +0530

    Add Header and Footer components, implement Button component, and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div> 
-      Home
+    <div className="flex flex-col">
+      <Header />
+      <section>
+        <div className="py-20">
+          <div className="flex flex-col items-center gap-6 justify-center">
+            <div className="text-black text-4xl font-bold">FAQ</div>
+            <div className="text-black text-2xl font-bold">
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-black">
+        <div className="text-white py-20">
+          <div className="flex flex-col items-center gap-6 justify-center">
+            <div className="text-4xl font-bold">Sign up today</div>
+            <Button className="bg-white text-black">Get Started</Button>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 }

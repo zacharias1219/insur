736fcadac148f6485c50e438c661c3b626ed29a9
Richard Abishai <rickoshade1891@gmail.com>
2024-11-05 22:24:11 +0530

    Add new pages for Claims, Overview, Policies, Support, and Transactions; update layout and navigation

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import MobileNav from '@/components/MobileNav'
+import Sidebar from '@/components/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
+        </div>
+      </div>
+      
+      <Toaster />
+    </main>
+  )
+}
+
+export default Layout
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+      label: "Overview",
+      route: "/overview",
+      icon: "/assets/icons/home.svg",
+    },
+    {
+      label: "Policies",
+      route: "/policies",
+      icon: "/assets/icons/image.svg",
+    },
+    {
+      label: "Claims",
+      route: "/claims",
+      icon: "/assets/icons/stars.svg",
+    },
+    {
+      label: "Transactions",
+      route: "/transactions",
+      icon: "/assets/icons/scan.svg",
+    },
+    {
+      label: "Support",
+      route: "/Support",
+      icon: "/assets/icons/filter.svg",
+    }
+  ];
